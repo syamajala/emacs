696bb378dcbcf9b86495898b53a9d1a99fc2d2b4
Venkata Yamajala <syamajala@gmail.com>
2018-01-27 08:02:42 -0500

    Update packages.

---
+++ elpa/epl-20180126.1213/epl-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "epl" "20180126.1213" "Emacs Package Library" '((cl-lib "0.3")) :commit "f412aa65814afe715523f77a0f2cba528284a50c" :url "http://github.com/cask/epl" :keywords '("convenience"))
---
+++ elpa/rtags-20180107.2358/rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rtags" "20180107.2358" "A front-end for rtags" 'nil :commit "53e74892e8bd15baa4d1bd1d640dcabcba9667ee" :url "http://rtags.net")
