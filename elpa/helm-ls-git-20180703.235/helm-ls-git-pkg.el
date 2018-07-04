e523587a4d105a194810638baefd9624ffe021c7
Seshu Yamajala <syamajala@gmail.com>
2018-07-04 06:47:53 -0400

    Update packages.

---
+++ elpa/helm-ls-git-20180703.235/helm-ls-git-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ls-git" "20180703.235" "list git files." '((helm "1.7.8")))
--- elpa/rtags-20180606.925/rtags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rtags" "20180606.925" "A front-end for rtags" 'nil :commit "f1f88750cb30c82e8e04d176f676ef5aa20ea458" :url "http://rtags.net")
