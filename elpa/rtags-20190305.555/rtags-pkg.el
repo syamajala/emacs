961ad946a7911020e4ec95ff4fd62afbfef5ddd6
Seshu Yamajala <syamajala@gmail.com>
2019-03-07 03:35:57 -0800

    Update packages.

---
+++ elpa/rtags-20190305.555/rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rtags" "20190305.555" "A front-end for rtags" 'nil :commit "38fcc0f27e39d084431ac1998e6d51352a86ad25" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
