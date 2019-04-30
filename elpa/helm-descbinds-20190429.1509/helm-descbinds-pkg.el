1bf73a686d9fc0ff5fbe7ca40d6550dc0b886067
Seshu Yamajala <syamajala@gmail.com>
2019-04-30 08:38:46 -0700

    Update packages.

---
+++ elpa/helm-descbinds-20190429.1509/helm-descbinds-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-descbinds" "20190429.1509" "A convenient `describe-bindings' with `helm'" '((helm "1.5")) :commit "13dbc2613c643f31f68498fe5d07f415376fb8a4" :keywords '("helm" "help") :authors '(("Taiki SUGAWARA" . "buzz.taiki@gmail.com")) :maintainer '("Taiki SUGAWARA" . "buzz.taiki@gmail.com") :url "https://github.com/emacs-helm/helm-descbinds")
